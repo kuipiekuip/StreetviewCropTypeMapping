dd7cc8f893cf200d88805f4dda98ecc44a3bf00f
Adrian <kuiper2000@hotmail.com>
2024-04-02 09:55:14 +0200

    only pull images within 3 months not accounting year

--- OSMRoadPoints/getStreetPoints.py
+++ OSMRoadPoints/getStreetPoints.py
@@ -7,6 +7,7 @@ from shapely.geometry import LineString, Point, Polygon
 from shapely.ops import nearest_points
 from pyproj import Transformer
 from tqdm import tqdm
+
 all_road_points = []
 
 # Constants
@@ -14,17 +15,20 @@ EARTH_RADIUS = 6371e3  # in meters
 DISTANCE_DELTA = 0.0001  # used for interpolating points along the road
 PERPENDICULAR_DISTANCE = 30  # distance for calculating field points
 OVERPASS_API_URL = "http://overpass-api.de/api/interpreter"
-SHAPEFILE_PATH = r'C:\Users\kuipe\OneDrive\Bureaublad\TU Delft\Master\Deep Learning\Project\StreetviewCropTypeMapping\data\bomen.shp'
+SHAPEFILE_PATH = r"C:\Users\kuipe\OneDrive\Bureaublad\TU Delft\Master\Deep Learning\Project\StreetviewCropTypeMapping\data\bomen.shp"
+
 
 # Change bearing because perpendicular
 def compute_bearing(from_point, to_point):
     """Calculate the bearing from one geographic point to another."""
     y = math.sin(to_point[1] - from_point[1]) * math.cos(to_point[0])
-    x = math.cos(from_point[0]) * math.sin(to_point[0]) - \
-        math.sin(from_point[0]) * math.cos(to_point[0]) * math.cos(to_point[1] - from_point[1])
+    x = math.cos(from_point[0]) * math.sin(to_point[0]) - math.sin(
+        from_point[0]
+    ) * math.cos(to_point[0]) * math.cos(to_point[1] - from_point[1])
     θ = math.atan2(y, x)
     bearing = (θ * 180 / math.pi + 360) % 360
     return bearing
+
 
 def compute_point_on_field(from_point, theta, distance):
     """Calculate a point at a certain distance and bearing from a given point."""
@@ -32,37 +36,46 @@ def compute_point_on_field(from_point, theta, distance):
     theta = math.radians(theta)
     lat1 = math.radians(from_point[0])
     lon1 = math.radians(from_point[1])
-    lat2 = math.asin(math.sin(lat1) * math.cos(angular_distance) + \
-                     math.cos(lat1) * math.sin(angular_distance) * math.cos(theta))
-    lon2 = lon1 + math.atan2(math.sin(theta) * math.sin(angular_distance) * math.cos(lat1),
-                             math.cos(angular_distance) - math.sin(lat1) * math.sin(lat2))
+    lat2 = math.asin(
+        math.sin(lat1) * math.cos(angular_distance)
+        + math.cos(lat1) * math.sin(angular_distance) * math.cos(theta)
+    )
+    lon2 = lon1 + math.atan2(
+        math.sin(theta) * math.sin(angular_distance) * math.cos(lat1),
+        math.cos(angular_distance) - math.sin(lat1) * math.sin(lat2),
+    )
     return (math.degrees(lat2), math.degrees(lon2))
+
 
 def process_shapefile(shapefile_path):
     """Process each geometry in the shapefile and save results."""
     geo_data = geopandas.read_file(shapefile_path)
-    geo_data = geo_data.dropna(subset=['CONDITIE']).reset_index(drop=True)
-    for geo_idx, geometry in tqdm(enumerate(geo_data.geometry),total=len(geo_data.geometry)):
+    geo_data = geo_data.dropna(subset=["CONDITIE"]).reset_index(drop=True)
+    for geo_idx, geometry in tqdm(
+        enumerate(geo_data.geometry), total=len(geo_data.geometry)
+    ):
         # if geo_data.iloc[geo_idx]['name_1'] == 'Andhra Pradesh':
         #     print("Geometry ", geo_data.iloc[geo_idx])
         # print('GEO Index ', geo_idx)
         if geo_idx >= 7:
             # print(geo_data['CONDITIE'][geo_idx])
             process_geometry(geometry, geo_idx, geo_data)
-            # if geo_idx == 200:
-            #     break
+            if geo_idx == 200:
+                break
+
 
 def process_geometry(geometry, geo_idx, geo_data):
     """Process a single geometry from the shapefile."""
     transformer = Transformer.from_crs(f"EPSG:28992", "EPSG:4326", always_xy=True)
     # Perform the transformation
     lon, lat = transformer.transform(geometry.x, geometry.y)
-    # geometry = Point(lon, lat) 
+    # geometry = Point(lon, lat)
     # lon , lat = geometry.x, geometry.y
     polygon_query = create_overpass_query(lon, lat)
     road_data = fetch_overpass_data(polygon_query)
 
     process_road_data(road_data, geo_idx, geo_data)
+
 
 def create_overpass_query(lon, lat):
     """Create an Overpass API query from exterior coordinates."""
@@ -76,42 +89,54 @@ def create_overpass_query(lon, lat):
 
     return overpass_query
 
+
 def fetch_overpass_data(query):
     """Fetch data from the Overpass API."""
-    response = requests.get(OVERPASS_API_URL, params={'data': query})
+    response = requests.get(OVERPASS_API_URL, params={"data": query})
     return response.json()
+
 
 def process_road_data(road_data, geo_idx, geo_data):
     """Process road data and save the output to CSV files."""
     road_points, field_points, original_points = [], [], []
 
-    for element in road_data['elements']:
-        if element['type'] == 'way':
-            keywords = ['highway']
-            tags = element.get('tags', {})
-            if  any(keyword in tags for keyword in keywords):
+    for element in road_data["elements"]:
+        if element["type"] == "way":
+            keywords = ["highway"]
+            tags = element.get("tags", {})
+            if any(keyword in tags for keyword in keywords):
                 try:
-                    process_way_element(element, road_points, field_points, original_points)
-                    
+                    process_way_element(
+                        element, road_points, field_points, original_points
+                    )
+
                 except Exception as e:
                     print(e)
 
     nearest_road_point_info = None
-    min_distance = float('inf')
+    min_distance = float("inf")
     for point in road_points:
         # Assuming point format is (x, y, bearing)
         x, y, bearing = point
         road_point = Point(x, y)
-        distance = geo_data['geometry'][geo_idx].distance(road_point)
+        distance = geo_data["geometry"][geo_idx].distance(road_point)
         if distance < min_distance:
             min_distance = distance
-            nearest_road_point_info = (x, y, bearing)  # Store the nearest point with its bearing
+            nearest_road_point_info = (
+                x,
+                y,
+                bearing,
+            )  # Store the nearest point with its bearing
 
     if nearest_road_point_info:
         # Append the nearest road point info, which includes bearing
-        all_road_points.append((geo_idx,) + nearest_road_point_info + (geo_data['CONDITIE'][geo_idx	],))
-    # if len(road_points) > 0:  
-    #     all_road_points.append((geo_idx,) + road_points[0] + (label,))           
+        all_road_points.append(
+            (geo_idx,)
+            + nearest_road_point_info
+            + (geo_data["CONDITIE"][geo_idx], geo_data["INSPECTIED"][geo_idx])
+        )
+    # if len(road_points) > 0:
+    #     all_road_points.append((geo_idx,) + road_points[0] + (label,))
 
     else:
         return
@@ -121,8 +146,8 @@ def process_road_data(road_data, geo_idx, geo_data):
 def process_way_element(element, road_points, field_points, original_points):
     """Process a single way element from the Overpass data."""
 
-    geo = element['geometry']
-    way = [(p['lat'], p['lon']) for p in geo]
+    geo = element["geometry"]
+    way = [(p["lat"], p["lon"]) for p in geo]
     original_points.extend(way)
     line = LineString(way)
     distances = np.arange(0, line.length, DISTANCE_DELTA)
@@ -136,35 +161,46 @@ def process_way_element(element, road_points, field_points, original_points):
     new_line = LineString(points)
     process_line_points(new_line, road_points, field_points)
 
+
 def process_line_points(line, road_points, field_points):
     """Process points along a line and compute adjacent field points."""
     old_x, old_y = None, None
 
     for j, (x, y) in enumerate(line.coords):
-        if j > 3 and j< len(line.coords)-3:
+        if j > 3 and j < len(line.coords) - 3:
 
             from_point = (old_x, old_y)
             to_point = (x, y)
             bearing = compute_bearing(from_point, to_point)
-            p1 = compute_point_on_field(to_point, (bearing + 90) % 360, PERPENDICULAR_DISTANCE)
-            p2 = compute_point_on_field(to_point, (bearing + 270) % 360, PERPENDICULAR_DISTANCE)
+            p1 = compute_point_on_field(
+                to_point, (bearing + 90) % 360, PERPENDICULAR_DISTANCE
+            )
+            p2 = compute_point_on_field(
+                to_point, (bearing + 270) % 360, PERPENDICULAR_DISTANCE
+            )
             field_points.append((p1[0], p1[1], (bearing + 90) % 360, x, y))
             field_points.append((p2[0], p2[1], (bearing + 270) % 360, x, y))
             road_points.append((x, y, bearing))
         old_x, old_y = x, y
 
+
 def save_to_csv(data, filename, header):
     """Save data to a CSV file using pandas for better handling of mixed data types."""
     # Convert data to DataFrame
     df = pd.DataFrame(data, columns=header.split(","))
-    
+
     # Save to CSV
     df.to_csv(filename, index=False)
+
 
 # Main execution
 if __name__ == "__main__":
     process_shapefile(SHAPEFILE_PATH)
     print(all_road_points[0])
     # save_to_csv(all_road_points, "roadPoints/allRoadPointswithGeo.csv", "geo_idx,y,x,b,x1,y1,x2,y2")
-    save_to_csv(all_road_points, "roadPoints/allRoadPoints.csv", "geo_index,rp_lat,rp_lon,b,label")
+    save_to_csv(
+        all_road_points,
+        "roadPoints/testRoadPoints.csv",
+        "geo_index,rp_lat,rp_lon,b,label,date",
+    )
     print("Done")
--- getGSVFieldImages.py
+++ getGSVFieldImages.py
@@ -13,37 +13,68 @@ import numpy as np
 import streetview
 import math
 from tqdm import tqdm
+from datetime import datetime
 
 KEY = """AIzaSyB-vx7Dh0LJ2abn8qDPeulCoc4d8w0kodM"""
 key = "&key=" + KEY
 
-TREECOVER_FILENAME = 'roadPoints/allRoadPoints.csv'
+TREECOVER_FILENAME = "roadPoints/testRoadPoints.csv"
 
 trees = pd.read_csv(TREECOVER_FILENAME)
 
-def checkInGrowing(date):
-    # print(date)
-    MONTHS = '08, 09, 10, 11, 12, 01,03'
-    # print(date[-2:])
-    if date[-2:] in MONTHS:
-        # print(date[-2:])
-        return True
-    else:
-        return False
 
-def getStreet(lat,lon,SaveLoc, bearing, meta):
+# def checkInGrowing(date, checkDate):
+#     print(date)
+#     MONTHS = "08, 09, 10, 11, 12, 01,03"
+#     # print(date[-2:])
+#     if date[-2:] in MONTHS:
+#         # print(date[-2:])
+#         return True
+#     else:
+#         return False
+
+
+def month_diff(month1, month2):
+    """Calculate the difference between two months, considering circular nature."""
+    diff = abs(month1 - month2)
+    return min(diff, 12 - diff)
+
+
+def checkInGrowing(image_date_str, check_date_str):
+    # Extracting month and year from the dates
+    image_date = datetime.strptime(image_date_str, "%Y-%m")
+    check_date = datetime.strptime(check_date_str, "%Y-%m-%d")
+    print(image_date, check_date)
+    # Calculate the month difference
+    diff = month_diff(image_date.month, check_date.month)
+    print(diff)
+    # Check if the difference is within 3 months
+    return diff <= 3
+
+
+def getStreet(lat, lon, SaveLoc, bearing, meta):
 
     if not os.path.exists(SaveLoc):
         os.makedirs(SaveLoc)
 
     # Zet conditie heirin de naam van de file
     heading1 = bearing
-    MyUrl = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location="+str(lat)+","+str(lon)+"&fov=90&heading="+str(heading1)+"&pitch=0" + key
+    MyUrl = (
+        "https://maps.googleapis.com/maps/api/streetview?size=640x640&location="
+        + str(lat)
+        + ","
+        + str(lon)
+        + "&fov=90&heading="
+        + str(heading1)
+        + "&pitch=0"
+        + key
+    )
     fi = meta + ".jpg"
-    urllib.request.urlretrieve(MyUrl, os.path.join(SaveLoc,fi))
+    urllib.request.urlretrieve(MyUrl, os.path.join(SaveLoc, fi))
+
 
 def getMeta(points, myloc, imLimit=0):
-    uniqueImageIDs= []
+    uniqueImageIDs = []
     points = points.reset_index()  # make sure indexes pair with number of rows
     if imLimit == 0:
         imLimit = len(points)
@@ -53,25 +84,33 @@ def getMeta(points, myloc, imLimit=0):
     for idx, tree in tqdm(points.iterrows(), total=len(points)):
         if i <= imLimit:
 
-            lon, lat = tree['rp_lon'], tree['rp_lat']
+            lon, lat = tree["rp_lon"], tree["rp_lat"]
             # print(lon, lat)
-            link = "https://maps.googleapis.com/maps/api/streetview/metadata?size=640x640&location="+str(lat)+","+str(lon)+"&fov=80&heading=0&pitch=0" + key
+            link = (
+                "https://maps.googleapis.com/maps/api/streetview/metadata?size=640x640&location="
+                + str(lat)
+                + ","
+                + str(lon)
+                + "&fov=80&heading=0&pitch=0"
+                + key
+            )
             response = requests.get(link)
             resJson = response.json()
             # print(resJson)
-            bearing = float(tree['b'])
+            bearing = float(tree["b"])
             # print(bearing)
-            if resJson['status'] ==  'OK':
-                if checkInGrowing(resJson['date']):
-                    if resJson['pano_id'] not in uniqueImageIDs:
-                        
-                        uniqueImageIDs.append(resJson['pano_id'])
-                        meta = str(tree['geo_index']) + '_' + str(tree['label']) 
+            if resJson["status"] == "OK":
+                if checkInGrowing(resJson["date"], tree["date"]):
+                    if resJson["pano_id"] not in uniqueImageIDs:
+
+                        uniqueImageIDs.append(resJson["pano_id"])
+                        meta = str(tree["geo_index"]) + "_" + str(tree["label"])
                         # print(meta)
-                        getStreet(lat,lon, myloc, bearing, meta)
+                        # getStreet(lat, lon, myloc, bearing, meta)
             else:
-                print('No images found')
-        i+=1
+                print("No images found")
+        i += 1
+
 
 imLimit = 100
-getMeta(trees, 'images', imLimit=0)
+getMeta(trees, "images", imLimit=0)
